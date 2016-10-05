fe036e3e7565dd23197014e1336b36e60599455d
andyadc <andaicheng@live.com>
2016-10-05 19:22:10 +0800

    optimize

--- spring-boot-hello/src/test/com/andyadc/study/springboot/test/HelloTests.java
+++ spring-boot-hello/src/test/com/andyadc/study/springboot/test/HelloTests.java
@@ -7,7 +7,6 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
