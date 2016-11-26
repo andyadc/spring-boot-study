8863d3548ab5ff40053c992763dfc4dbedaad7a4
andyadc <andaicheng@gmail.com>
2016-11-26 22:03:10 +0800

    springboot quickstart

---
+++ spring-boot-quickstart/src/main/java/com/andyadc/study/springboot/quickstart/App.java
@@ -0,0 +1,13 @@
+package com.andyadc.study.springboot.quickstart;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
