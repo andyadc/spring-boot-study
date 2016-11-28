26ca0dcd0ae45d8858108ee35a55715e94fd30e4
andaicheng <andaicheng@gmail.com>
2016-11-28 14:46:39 +0800

    spring boot quickstart

---
+++ spring-boot-quickstart/src/main/java/com/andyadc/study/springboot/quickstart/MySpringBootApplication.java
@@ -0,0 +1,16 @@
+package com.andyadc.study.springboot.quickstart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author andaicheng
+ * @version 2016/11/28
+ */
+@SpringBootApplication
+public class MySpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MySpringBootApplication.class, args);
+    }
+}
