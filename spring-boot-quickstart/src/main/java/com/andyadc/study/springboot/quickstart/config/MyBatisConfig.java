d196ea48db47d81405268f0124d96a7431dea2fd
andaicheng <andaicheng@gmail.com>
2016-11-28 18:22:12 +0800

    MyBatisConfig DataSource

--- spring-boot-quickstart/src/main/java/com/andyadc/study/springboot/quickstart/config/MyBatisConfig.java
+++ spring-boot-quickstart/src/main/java/com/andyadc/study/springboot/quickstart/config/MyBatisConfig.java
@@ -1,12 +1,18 @@
 package com.andyadc.study.springboot.quickstart.config;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.transaction.annotation.TransactionManagementConfigurer;
+
+import javax.sql.DataSource;
 
 /**
  * DataSource、SqlSessionFactory和Transaction Manager 配置
@@ -28,6 +34,20 @@ public class MyBatisConfig implements TransactionManagementConfigurer {
     private String username;
     @Value("${spring.datasource.password}")
     private String password;
+
+
+    @Bean(destroyMethod = "close")
+    @Primary
+    public DataSource dataSource(){
+        HikariConfig config = new HikariConfig();
+        config.setDriverClassName(driverClass);
+        config.setJdbcUrl(jdbcUrl);
+        config.setUsername(username);
+        config.setPassword(password);
+
+        HikariDataSource dataSource = new HikariDataSource(config);
+        return dataSource;
+    }
 
 
     @Override
