2f704f46af9ebc51e7d9a38f8363468e25348799
andyadc <andaicheng@gmail.com>
2016-12-01 22:24:33 +0800

    Scheduler

--- spring-boot-quickstart/src/main/java/com/andyadc/study/springboot/quickstart/MySpringBootApplication.java
+++ spring-boot-quickstart/src/main/java/com/andyadc/study/springboot/quickstart/MySpringBootApplication.java
@@ -2,12 +2,14 @@ package com.andyadc.study.springboot.quickstart;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * @author andaicheng
  * @version 2016/11/28
  */
 @SpringBootApplication
+@EnableScheduling
 public class MySpringBootApplication {
 
     public static void main(String[] args) {
