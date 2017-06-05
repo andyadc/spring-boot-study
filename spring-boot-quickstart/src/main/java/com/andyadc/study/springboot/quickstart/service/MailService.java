5256e1c9d2560397dc216c9ac44386bf209f8d4d
andaicheng <andaicheng@gmail.com>
2017-06-05 20:52:37 +0800

    MailService

---
+++ spring-boot-quickstart/src/main/java/com/andyadc/study/springboot/quickstart/service/MailService.java
@@ -0,0 +1,11 @@
+package com.andyadc.study.springboot.quickstart.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * @author andaicheng
+ * @version 2017/6/5
+ */
+@Service
+public class MailService {
+}
