c196e79ed3c7841deb57766e9f307dedbd9bb78b
andaicheng <andaicheng@gmail.com>
2017-06-05 21:00:34 +0800

    Mail Send

--- spring-boot-quickstart/src/main/java/com/andyadc/study/springboot/quickstart/service/MailService.java
+++ spring-boot-quickstart/src/main/java/com/andyadc/study/springboot/quickstart/service/MailService.java
@@ -1,5 +1,11 @@
 package com.andyadc.study.springboot.quickstart.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 /**
@@ -8,4 +14,34 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class MailService {
+
+    private static final Logger LOG = LoggerFactory.getLogger(MailService.class);
+
+    @Autowired
+    private JavaMailSender mailSender;
+
+    @Value("${spring.mail.username}")
+    private String from;
+
+    /**
+     * 发送纯文本的简单邮件
+     *
+     * @param to
+     * @param subject
+     * @param content
+     */
+    public void sendSimpleMail(String to, String subject, String content) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom(from);
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(content);
+
+        try {
+            mailSender.send(message);
+            LOG.info("简单邮件已经发送。");
+        } catch (Exception e) {
+            LOG.error("发送简单邮件时发生异常！", e);
+        }
+    }
 }
---
+++ spring-boot-quickstart/src/test/java/com/andyadc/study/springboot/quickstart/MailServiceTest.java
@@ -0,0 +1,25 @@
+package com.andyadc.study.springboot.quickstart;
+
+import com.andyadc.study.springboot.quickstart.service.MailService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * @author andaicheng
+ * @version 2017/6/5
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = {Startup.class})
+public class MailServiceTest {
+
+    @Autowired
+    private MailService mailService;
+
+    @Test
+    public void testMailSend() {
+        mailService.sendSimpleMail("andaicheng@qq.com", "mail", "this is from spring boot");
+    }
+}
