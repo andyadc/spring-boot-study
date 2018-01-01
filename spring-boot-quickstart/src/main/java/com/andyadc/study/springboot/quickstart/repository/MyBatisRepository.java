1b3d11d973450be9e1d109c8b52b317f3b424584
andyadc <andaicheng@gmail.com>
2018-01-01 11:46:08 +0800

    Spring Boot

--- spring-boot-quickstart/src/main/java/com/andyadc/study/springboot/quickstart/repository/MyBatisRepository.java
+++ spring-boot-quickstart/src/main/java/com/andyadc/study/springboot/quickstart/repository/MyBatisRepository.java
@@ -1,6 +1,10 @@
 package com.andyadc.study.springboot.quickstart.repository;
 
-import java.lang.annotation.*;
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 /**
  * @author andaicheng
